abd5ffb8250e181f1f9d2a48f4131c72d723c1b3
sagorranait <sagorranait@gmail.com>
2023-10-10 16:16:04 +0600

    Design of Number Input is done

--- screens/Login.js
+++ screens/Login.js
@@ -1,8 +1,11 @@
-import { View, Text, Image, StyleSheet } from 'react-native';
+import { View, Text, Image, StyleSheet, TextInput } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import FONTS from '../constants/fonts'
+import { COLORS } from '../constants';
 
 export default function Login() {
+  const onLogin = () => {}
+
   return (
     <SafeAreaView>
       <View style={styles.container}>
@@ -15,16 +18,22 @@ export default function Login() {
           <Text style={styles.logoText}>Techno EDGE Network Solution</Text>
           <Text style={styles.subTitle}>24/7 WiFi Connection</Text>
         </View>
-        <View>
-          <Text>Phone Number</Text>
-          <View>
-            <View>
+        <View style={styles.inputArea}> 
+          <Text style={styles.inputTitle}>Use the phone number you chose when registering to your account.</Text>
+          <View style={[styles.inputContainer, styles.inputBorder]}>
+            <View style={styles.inputContainer}>
               <Image
                 source={require('../assets/bd-icon.png')}
-                style={styles.logo}
+                style={styles.inputLogo}
               />
-              <Text>+880</Text>
+              <Text style={styles.inputLogoText}>+880</Text>
             </View>
+            <TextInput
+              style={[styles.inputLogoText, styles.numberInput]}
+              onChangeText={onLogin}
+              placeholder="Phone Number"
+              keyboardType="numeric"
+            />
           </View>
         </View>
       </View>
@@ -53,5 +62,36 @@ const styles = StyleSheet.create({
   subTitle: {
     fontFamily: FONTS.Poppins,
     fontSize: 20
+  },
+  inputArea: {
+    paddingTop: 50
+  },
+  inputTitle: {
+    fontFamily: FONTS.Poppins,
+    fontSize: 15,
+    paddingBottom: 15
+  },
+  inputContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    gap: 10,
+  },
+  inputLogo:{
+    width: 45,
+    height: 45,
+    objectFit: 'contain'
+  },
+  inputLogoText:{
+    fontFamily: FONTS.Poppins,
+    fontSize: 16
+  },
+  numberInput: {
+    flex: 1
+  },
+  inputBorder: { 
+    paddingBottom: 5, 
+    borderBottomWidth: 1, 
+    borderBottomColor: COLORS.gray
   }
 });
